3664394d8b76560dbb8d74267c4aaef2f5f1b031
Hoàng Tiến Sơn <77462107+hisonthanhhoa@users.noreply.github.com>
2022-07-12 00:20:36 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    introTitle: 'Hello !',
+    introDesc: `Trái đất này hơi tròn, tự nhiên lại gặp
+    được Hoa, thích ghê`,
+    btnIntro: 'hihi',
+    title: 'Hoa có muốn nhắn tin với Sơn tiếp không 😙',
+    desc: 'Đừng có tự nhiên biến mất nhía ',
+    btnYes: 'Không hề biến mất <33',
+    btnNo: 'Biến đi liền :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao lại gặp Hoa',
+    btnReply: 'Gửi cho Sơn <3',
+    reply: 'Tại ông trời <33333333',
+    mess: 'Sơn biết mà 🥰. hihi😘😘',
+    messDesc: 'Hẹn gặp ở Hà Nội nhé .',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100009270018523'
+}
